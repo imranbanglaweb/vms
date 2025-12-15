75d608ae2cb3c72f9e09d8406b56d7fb4488f659
imranbanglaweb <md.imran1200@gmail.com>
2025-12-15 18:01:01 +0600

    dsd

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,9 @@
-self.addEventListener('push', function (event) {
+self.addEventListener('push', function(event) {
     const data = event.data.json();
-
     event.waitUntil(
         self.registration.showNotification(data.title, {
             body: data.body,
-            icon: '/icon.png',
-            badge: '/badge.png',
-            data: data.url
+            icon: data.icon
         })
-    );
-});
-
-self.addEventListener('notificationclick', function (event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow(event.notification.data)
     );
 });
