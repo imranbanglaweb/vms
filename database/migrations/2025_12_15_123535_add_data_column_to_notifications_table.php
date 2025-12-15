cd5a87b7f8863c36c50719527d80ad3451b73686
imranbanglaweb <md.imran1200@gmail.com>
2025-12-15 12:39:53 +0600

    sdd

--- database/migrations/2025_12_15_123535_add_data_column_to_notifications_table.php
+++ database/migrations/2025_12_15_123535_add_data_column_to_notifications_table.php
@@ -17,7 +17,6 @@ class AddDataColumnToNotificationsTable extends Migration
              $table->json('data')->nullable()->after('type');
         });
     }
-
     /**
      * Reverse the migrations.
      *
